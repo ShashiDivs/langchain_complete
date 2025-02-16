5276fc63b21b66c963bf9a4120a0552bceeb476a
shashi <shashipolity@gmail.com>
2025-02-16 11:44:59 +0530

    basic chat models files added

---
+++ 1_chat_models/1_chat_model_basic.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4o")
+
+result = model.invoke("What is 81 divided by 9?")
+print("Full result:")
+print(result)
+print("Content only:")
+print(result.content)
